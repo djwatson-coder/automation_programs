721cf6b02257577fb64896945e635bbc1098eea3
djwatson-coder <65384624+djwatson-coder@users.noreply.github.com>
2022-02-22 22:52:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 
-import settings
+from settings import automation_program, run_type, selection_points
+from taxbot import TaxBot
+from folderdataentry import FolderDataEntry
 import keymouse
 import folderdataentry
 
@@ -7,18 +9,22 @@ import folderdataentry
 # Select the automation_Script to Run
 def main():
 
-    if settings.automation_program == "FolderDataEntry":
-        bot = folderdataentry.FolderDataEntry()
+    if automation_program == "FolderDataEntry":
+        bot = FolderDataEntry()
         points = {'home': (-1388, 1455), 'selection1': (-1041, 558), 'selection2': (-1041, 558)}
         bot.run(points=points)
+
+    if automation_program == "TaxBot":
+        bot = TaxBot()
+        bot.run()
 
     return
 
 
 if __name__ == '__main__':
-    if settings.run_type == "sel_extract":
+    if  run_type == "sel_extract":
         km = keymouse.KeyMouse()
-        km.click_coordinates(settings.selection_points)
+        km.click_coordinates(selection_points)
     else:
         main()
 
