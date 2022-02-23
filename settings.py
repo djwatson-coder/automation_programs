787d98cdad44f3f696a759e3654c39767764e59c
djwatson-coder <65384624+djwatson-coder@users.noreply.github.com>
2022-02-22 22:52:47 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,4 +4,4 @@ excel_file_path = ""
 run_type = "automate"  # sel_extract / automate
 selection_points = ["home", "selection1", "selection2"]
 
-automation_program = "FolderDataEntry"
+automation_program = "TaxBot"  # TaxBot / FolderDataEntry
