1b7130d362f6167fd2c510e0de059dc2b9fe01ff
djwatson-coder <65384624+djwatson-coder@users.noreply.github.com>
2022-02-23 11:39:14 -0500

    Update automation.py

--- automation.py
+++ automation.py
@@ -1,3 +1,8 @@
+import os
+from pathlib import Path
+import shutil
+import win32com.client
+
 
 class Automation:
     def __init__(self):
@@ -5,3 +10,45 @@ class Automation:
 
     def run(self, **kwargs):
         return
+
+    @staticmethod
+    def check_directory(directory, path_to_check):
+        """ Checks if the folder/file exists in the directory"""
+        for folder in os.listdir(directory):
+            if folder.startswith(path_to_check):
+                return folder
+        return False
+
+    @staticmethod
+    def create_directory(directory):
+        Path(directory).mkdir(parents=True, exist_ok=True)
+
+    def get_matching_files(self, matching_string):
+        files = []
+        for file in os.listdir(self.source_path):
+            if file.startswith(matching_string):
+                files.append(file)
+
+        return files
+
+    @staticmethod
+    def move_files(path, destination, files):
+        for file in files:
+            if os.path.isfile(f"{destination}/{file}"):
+                os.remove(f"{destination}/{file}")
+            # shutil.move(os.path.join(path, file), destination)
+            shutil.copy(os.path.join(path, file), destination)
+
+    @staticmethod
+    def send_email(**kwargs):
+
+        outlook = win32com.client.Dispatch('outlook.application')
+        mail = outlook.CreateItem(0)
+
+        mail.To = kwargs["to_address"]
+        mail.Subject = kwargs['subject']
+        # mail.HTMLBody = kwargs['html_body']
+        mail.Body = kwargs['body']
+        for attachment in kwargs["attachment_files"]:
+            mail.Attachments.Add(kwargs["attachment_file_path"] + "/" + attachment)
+        mail.Send()
