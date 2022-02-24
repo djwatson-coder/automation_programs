9706bd36f738acd0b7c958101e976a6102b928af
djwatson-coder <65384624+djwatson-coder@users.noreply.github.com>
2022-02-24 16:25:27 -0500

    Update taxbot.py

--- taxbot.py
+++ taxbot.py
@@ -1,13 +1,14 @@
-import os
 import time
-import re
-import fitz
 from PyPDF2 import PdfFileWriter, PdfFileReader
 from pikepdf import Pdf
 from automation import Automation
 
 
 class TaxBot(Automation):
+    """
+    Tax Bot Takes Personal Tax Files, stores the releveant files in the correct client location and
+    sends an email to the Client partner with the attachments and formatted message to the client.
+    """
     def __init__(self):
         super().__init__()
 
@@ -16,30 +17,38 @@ class TaxBot(Automation):
         #self.source_path = "L:\Toronto\T1\T1-2020\PDF"
         #self.source_path =
 
+        self.toronto_personal_client_dir = "I:/Toronto/_Personal Tax - TOR"
+        self.vancouver_personal_client_dir = "I:/Vancouver/_Personal Tax - VAN"
+
         # This should be a csv/text file that's written to but not cleared
-        self.already_completed = []
+        self.completed_entities = []
+        self.tax_prep_string = "00"
 
     def run(self):
-
+        """
+        Checks the folder to see if there are any tax prep files that have not been stored and sent.
+        If there are any it runs the storing and sending process.
+        :return: None
+        :rtype: None
+        """
         while True:
-            file_path = self.check_input_folder("00")
-            if file_path:
-                file = file_path[1]
-                if not self.run_process(file):
-                    return
-                else:
-                    self.already_completed.append(file)
-                break  # for now - remove when wanting to run multiple
+            file = self.check_input_folder(self.tax_prep_string)
+            if file:
+                print("################### BEGIN TAXBOT PROCESS ###################")
+                print(f"File Found: {file}")
+                if self.run_process(file):
+                    self.completed_entities.append(file)
+                    print("#################################################################")
             else:
                 # wait 60 seconds
                 time.sleep(60)
+
+
 
     def run_process(self, document_00):
 
         # 0. Read in the pdf and set the name variables ####
         first_name, last_name, sin, email = self.read_taxprep_pdf(document_00)
-
-        return False
 
         sin = sin.replace(' ', '')
         sin_pw = sin
@@ -49,9 +58,6 @@ class TaxBot(Automation):
         year = 2021
         output_folder = "_FINAL T1 DOCS"
 
-        # path_name = "Holman, Kimberly"
-        # name = "Holman_Kimberly_A"
-
         # 1. Create destination path: ####
         destination_path, city = self.select_directory(client_folder_path, year, last_init, output_folder)
         if not destination_path:
@@ -59,123 +65,89 @@ class TaxBot(Automation):
             return
 
         print(f"Destination path successfully found for {client_folder_path}")
-        # self.destination_path = "Q:/Admin - Digital Technology and Risk Advisory/Tax Bot/Tax Test/Test Folder"
-        # self.destination_path = "I:/Toronto/_Personal Tax - TOR/H/Holman, Kimberly_94214/2020/CRA"
-        return False
 
         # 2. move the pdf files ####
-
-        # file_1 = file_name + f"_1-Ltr_{year}.pdf"
-        # file_2 = file_name + f"_2-T1_({city})_{year}.pdf"
-        # file_3 = file_name + f"_3-Docs_Needing_Signature_{year}.pdf"
-        # files = [file_1, file_2, file_3]
-
         files = self.get_matching_files(file_name)
         self.move_files(self.source_path, destination_path, files)
 
+        print(f"FILES MOVED TO: {destination_path}")
+
         # 3. Decrypt the letter pdf and extract the information
         letter_name = file_name + f"_1-Ltr_{year - 1}.pdf"
-        # letter_name = file_name + f"_2-T1_({city})_{year -1}.pdf"
-
         self.decrypt_pdf(destination_path, letter_name, sin_pw)
-
         partner, total = self.get_letter_info(destination_path, letter_name)
-        partner_email = self.get_partner_email(partner)
 
-        return
-
-        # 3. Send the email with attachments
-        to_address = "david.watson@bakertillywm.com"
-        # to_address = "deepak.upadhyaya@bakertillywm.com"
+        # 4. Send the email with attachments
+        email_string = "@bakertillywm.com"
+        to_address = f"david.watson{email_string}" # deepak.upadhyaya
         subject = f"Tax report for: {first_name.split(' ')[0]}"
         html_body = '<h3>This is HTML Body</h3>'
-        body = f"Send to {email} from {partner_email}\n\n" \
+        body = f"Send to {email} from {partner.replace(' ', '.').lower() + email_string}\n\n" \
                "Dear Kimberly,\n\n" \
                f"Please find attached your tax return report. The total amount owing is {total}\n\n" \
                "Kind Regards"
-        print(body)
 
         self.send_email(to_address=to_address,
                         subject=subject,
                         html_body=html_body,
                         body=body,
                         attachment_files=files,
-                        attachment_file_path=destination_path
-                        )
-        print("email sent")
+                        attachment_file_path=destination_path,
+                        send=False)
 
-        return
+        print("EMAIL SENT TO:")
+        print(f"    Partner: {partner} at: {partner.replace(' ', '.').lower() + email_string}")
+        print(f"Process Complete for: {first_name} {last_name}")
+
+
+        return True
 
     def read_taxprep_pdf(self, document_00):
-        with fitz.open(f"{self.source_path}/{document_00}") as doc:
-            text = ""
-            for page in doc:
-                text += page.getText()
 
-        text_list = text.split("\n")
-
-        sin_place = 0
-        email_place = 0
-        for idx, line in enumerate(text_list):
-            print(f"{idx}: {line}")
-            if self.sin_match(line) and sin_place == 0:
-                sin_place = idx
-            if self.email_match(line) and email_place == 0:
-                email_place = idx
-
-        sin = text_list[sin_place]
-        first_name = text_list[sin_place-2]
-        last_name = text_list[sin_place-1]
-        email = text_list[email_place]
+        text_list = self.get_pdf_as_list(f"{self.source_path}/{document_00}")
+        # self.read_out_pdf_list(text_list)
+        sin_place, sin = self.find_first_pattern(text_list, "[0-9]{3} [0-9]{3} [0-9]{3}")
+        email_place, email = self.find_first_pattern(text_list, "[0-z]*@[0-z]*(.com)")
         if email_place == 0:
             email = "No Email"
+        first_name = text_list[sin_place - 2]
+        last_name = text_list[sin_place - 1]
 
-        print(f"{first_name}, {last_name}: {sin}, {email}")
+        print(f"    Name:{first_name} {last_name}\n"
+              f"    SIN: {sin}\n"
+              f"    Email: {email}")
 
         return first_name, last_name, sin, email
 
-    def read_out_pdf(self):
-
-        with fitz.open(self.source_path + "/Holman_Kimberly_A_1-Ltr_2020.pdf") as doc:
-            text = ""
-            for page in doc:
-                text += page.getText()
-
-        text_list = text.split("\n")
-        for idx, line in enumerate(text_list):
-            print(f"{idx}: {line}")
-
-        return
-
     def select_directory(self, path_name, year, last_init, folder):
 
-        tor = self.check_directory(f"I:/Toronto/_Personal Tax - TOR/{last_init}", path_name)
-        van = self.check_directory(f"I:/Vancouver/_Personal Tax - VAN/{last_init}", path_name)
+        tor = self.fuzzy_check_directory(f"{self.toronto_personal_client_dir}/{last_init}", path_name)
+        van = self.fuzzy_check_directory(f"{self.vancouver_personal_client_dir}/{last_init}", path_name)
+
         if van:
-            path = f"I:/Vancouver/_Personal Tax - VAN/{last_init}/{van}/{year}/{folder}"
+            path = f"{self.vancouver_personal_client_dir}/{last_init}/{van}/{year}/{folder}"
             self.create_directory(path)
             city = "Vancouver"
         elif tor:
-            path = f"I:/Toronto/_Personal Tax - TOR/{last_init}/{tor}/{year}/{folder}"
+            path = f"{self.toronto_personal_client_dir}/{last_init}/{tor}/{year}/{folder}"
             self.create_directory(path)
             city = "Toronto"
         else:
-            return False, None
+            raise NotADirectoryError("No File Path for the client at either:\n"
+                            f"{self.toronto_personal_client_dir}/{last_init}/{path_name}\n"
+                            f"{self.vancouver_personal_client_dir}/{last_init}/{path_name}")
 
         return path, city
 
     def decrypt_pdf(self, path, file, password):
 
-        out = PdfFileWriter()
         if not self.check_directory(path, file):
-            print("No Cover Letter Found")
-            return
+            raise NotADirectoryError(f"No File Path for the client at :\n{path}/{file}")
 
         pdf_file = PdfFileReader(f"{path}/{file}")
 
         # Check if the opened file is actually Encrypted
         if pdf_file.isEncrypted:
-            print(password)
 
             with Pdf.open(f"{path}/{file}", password=password, allow_overwriting_input=True) as pdf:
                 pdf.save(f"{path}/{file}")
@@ -184,21 +156,13 @@ class TaxBot(Automation):
         else:
             print("File already decrypted.")
 
-    @staticmethod
-    def get_letter_info(destination_path, letter_name):
-        with fitz.open(f"{destination_path}/{letter_name}") as doc:
-            text = ""
-            for page in doc:
-                text += page.getText()
-
-        text_list = text.split("\n")
-        for idx, line in enumerate(text_list):
-            print(f"{idx}: {line}")
+    def get_letter_info(self, path, letter_name):
+        text_list = self.get_pdf_as_list(f"{path}/{letter_name}")
+        # self.read_out_pdf_list(text_list)
         total_amount = text_list[51]
-        partner_line = text_list[65].split(' ')
-        print(partner_line)
+        idx, partner_line = self.find_first_pattern(text_list, "Per: ")
+        partner_line = partner_line.split(' ')
         partner = f"{partner_line[1]} {partner_line[2].replace(',','')}"
-        print(partner)
 
         return partner, total_amount
 
@@ -214,24 +178,22 @@ class TaxBot(Automation):
         files_to_process = []
 
         for file in files:
-            if file not in self.already_completed:
+            if file not in self.completed_entities:
                 files_to_process.append(file)
 
-        if len(files) == 0:
+        if len(files_to_process) == 0:
             return False
         else:
-            return files
-    @staticmethod
-    def sin_match(item):
-        pattern = re.compile("[0-9]{3} [0-9]{3} [0-9]{3}")
-        return pattern.match(item)
+            return files_to_process[0]
 
-    @staticmethod
-    def email_match(item):
-        pattern = re.compile("[0-z]*@[0-z]*(.com)")
-        return pattern.match(item)
+
+
 
 
 # ToDo Test the pdf decrypter with the full SIN
 # ToDo Create partner lookup table
 # ToDo Add client ID to the folder name
+# ToDo robustness checks and exception handling
+# ToDo get the accurate place of the partner and amounts in the letter
+# ToDo find the dependencies through print to send an email only to the first name
+
