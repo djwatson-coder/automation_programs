c2abfea2c9c7b099efe5f75ca252f2aead553e2d
djwatson-coder <65384624+djwatson-coder@users.noreply.github.com>
2022-03-01 09:28:24 -0500

    Create tbsettings.py

---
+++ taxbot/tbsettings.py
@@ -0,0 +1,22 @@
+
+# File Paths
+source_path = "Q:/Admin - Digital Technology and Risk Advisory/Tax Bot/Tax Test/Test Folder"
+# self.source_path = "L:\Toronto\T1\T1-2020\PDF"
+manual_to_do_path = "Q:/Admin - Digital Technology and Risk Advisory/Tax Bot/Tax Test/" \
+                         "Test Folder/Manual_to_do"
+
+toronto_personal_client_dir = "I:/Toronto/_Personal Tax - TOR"
+vancouver_personal_client_dir = "I:/Vancouver/_Personal Tax - VAN"
+
+
+completed_entities = []  # stored in an Excel file after each iteration
+tax_prep_string = "00_"
+
+slp = 30  # length of time to sleep for
+remove_files = True  # Remove the files from the source directory
+
+
+# Email Settings
+enable_emailing = False
+email_partner = False
+email_string = "@bakertillywm.com"
