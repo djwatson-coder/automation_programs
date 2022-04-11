1636a111ff22f11c9cbe852d7dd5f50389add734
djwatson-coder <65384624+djwatson-coder@users.noreply.github.com>
2022-04-11 13:05:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,42 +2,23 @@
 from settings import automation_program, run_type
 from taxbotfiles.taxbot import TaxBot
 from folderdataentry import FolderDataEntry
-from taxbotfiles.tbot import TaxBotTesting
-from taxbotfiles.gui import GUI
-import pickle
-
 
 # Select the automation_Script to Run
-def main():
+def main(**kwargs):
     if automation_program == "FolderDataEntry":
         bot = FolderDataEntry()
         points = {'home': (-1388, 1455), 'selection1': (-1041, 558), 'selection2': (-1041, 558)}
         bot.run(points=points)
 
     if automation_program == "TaxBot":
-        bot = TaxBot()
+        bot = TaxBot(location)
         bot.run()
 
     return
 
 
-def create_pickle():
-    outfile = open("sample.pkl", 'wb')
-    pickle.dump([], outfile)
-    outfile.close()
-
-
 if __name__ == '__main__':
 
-    if run_type == "testing":
-        testing = TaxBotTesting()
-        testing.test_something()
-    else:
-        # create_pickle()
-        main()
+    location = "VAN"
+    main(location=location)
 
-
-
-# ToDo Create access to mouse click
-# ToDo Create access to keyboard
-# ToDo Create ability to read in Excel files
