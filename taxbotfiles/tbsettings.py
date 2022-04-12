03e0e2799de317627551ca6658c1be2c5712a3ee
djwatson-coder <65384624+djwatson-coder@users.noreply.github.com>
2022-04-12 12:10:50 -0700

    Update file paths

--- taxbotfiles/tbsettings.py
+++ taxbotfiles/tbsettings.py
@@ -1,19 +1,21 @@
 
 # FILE PATHS ---
-source_path_tor = "Q:/Admin - Digital Technology and Risk Advisory/Tax Bot/Tax Test/Test Folder"
-source_path_van = "Q:/Admin - Digital Technology and Risk Advisory/Tax Bot/Tax Test/Test Folder 2"
+client_info_file = "//tor-fs01/TAXPREP/Vancouver/T1/T1-2021/PDF/David W/T1-Clients-All-Offices.csv"
+
+source_path_tor = "Q:/Admin - Digital Technology and Risk Advisory/Tax Bot/Tax Test/Test Folder 2"
+source_path_van = "Q:/Admin - Digital Technology and Risk Advisory/Tax Bot/Tax Test/Test Folder"
 # source_path_tor = "//tor-fs01/TAXPREP/Toronto/T1/T1-2021/PDF"
 # source_path_van = "//van-fs01/TAXPREP/Vancouver/T1/T1-2021/PDF"
 
-completed_folder_tor = "Q:/Admin - Digital Technology and Risk Advisory/Tax Bot/Tax Test/Test Folder/bot-completed"
-issues_folder_tor = "Q:/Admin - Digital Technology and Risk Advisory/Tax Bot/Tax Test/Test Folder/bot-issues"
-completed_folder_van = "Q:/Admin - Digital Technology and Risk Advisory/Tax Bot/Tax Test/Test Folder 2/bot-completed"
-issues_folder_van = "Q:/Admin - Digital Technology and Risk Advisory/Tax Bot/Tax Test/Test Folder 2/bot-issues"
+completed_folder_tor = "Q:/Admin - Digital Technology and Risk Advisory/Tax Bot/Tax Test/Test Folder 2/bot-completed"
+issues_folder_tor = "Q:/Admin - Digital Technology and Risk Advisory/Tax Bot/Tax Test/Test Folder 2/bot-issues"
+completed_folder_van = "Q:/Admin - Digital Technology and Risk Advisory/Tax Bot/Tax Test/Test Folder/bot-completed"
+issues_folder_van = "Q:/Admin - Digital Technology and Risk Advisory/Tax Bot/Tax Test/Test Folder/bot-issues"
 
 # completed_folder_tor = "//tor-fs01/TAXPREP/Toronto/T1/T1-2021/PDF/bot-completed"
 # issues_folder_van = "//tor-fs01/TAXPREP/Toronto/T1/T1-2021/PDF/bot-issues"
-# completed_folder_van = "//tor-fs01/TAXPREP/Toronto/T1/T1-2021/PDF/bot-completed"
-# issues_folder_van = "//tor-fs01/TAXPREP/Toronto/T1/T1-2021/PDF/bot-issues"
+# completed_folder_van = "//tor-fs01/TAXPREP/Vancouver/T1/T1-2021/PDF/bot-completed"
+# issues_folder_van = "//tor-fs01/TAXPREP/Vancouver/T1/T1-2021/PDF/bot-issues"
 
 toronto_personal_client_dir = "I:/Toronto/_Personal Tax - TOR"
 vancouver_personal_client_dir = "I:/Vancouver/_Personal Tax - VAN"
@@ -21,9 +23,9 @@ vancouver_personal_client_dir = "I:/Vancouver/_Personal Tax - VAN"
 
 completed_entities = []  # stored in an Excel file after each iteration
 
-tax_prep_string = "00_"
+tax_prep_string = "_00-ID"
 output_folder = "_FINAL T1 DOCS"
-year = 2021
+yr = 2021
 
 slp = 30  # length of time to sleep for
 remove_files = False  # Remove the files from the source directory
